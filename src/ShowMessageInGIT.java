96265ae361d0b6432f25bcf8e25637175ea0eba0
Ytallo Gadelha <ytallolg@gmail.com>
2016-04-21 20:37:19 -0300

    mudando Y para y na primeira mensagem

--- src/ShowMessageInGIT.java
+++ src/ShowMessageInGIT.java
@@ -3,7 +3,7 @@ public class ShowMessageInGIT {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Testando branch Ytallo");
+		System.out.println("Testando branch ytallo");
 		System.out.println("Teste OK");
 
 	}
