c2a4717ede36ecf2e1a383ad9f7725aeb9fa6654
Ytallo Gadelha <ytallolg@gmail.com>
2016-04-21 16:33:19 -0300

    alteração na branch raphael

--- src/ShowMessageInGIT.java
+++ src/ShowMessageInGIT.java
@@ -3,7 +3,7 @@ public class ShowMessageInGIT {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Testando GIT");
+		System.out.println("Testando raphael");
 
 	}
 
