a03ad8773b82f3d4261989f65fbcad45baa79c27
raphael4ndrade <raphael4ndrade@gmail.com>
2016-04-21 17:30:20 -0300

    Alterção do ytallo elderys...

--- src/ShowMessageInGIT.java
+++ src/ShowMessageInGIT.java
@@ -3,8 +3,8 @@ public class ShowMessageInGIT {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("Testando ytallo");
+		System.out.println("Testando elderys ytallus");
+		System.out.println("Testando Ok!");
 
 	}
 
