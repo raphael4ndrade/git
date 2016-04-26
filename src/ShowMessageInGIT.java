a8c1d675462c3d5cabddb8a17689be18a34694cd
Ytallo Gadelha <ytallolg@gmail.com>
2016-04-26 08:57:43 -0300

    enviando branch raphael

--- src/ShowMessageInGIT.java
+++ src/ShowMessageInGIT.java
@@ -3,8 +3,7 @@ public class ShowMessageInGIT {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Testando branch raphael");
-		System.out.println("Teste OK");
+		System.out.println("enviando branch raphael");
 
 	}
 
